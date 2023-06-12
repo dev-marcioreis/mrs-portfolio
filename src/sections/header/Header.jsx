660e89773c8b5da30c84447576608ff33eccc73d
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-06-12 16:51:59 -0300

    Feat: starting the header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,10 +1,30 @@
 import './header.css'
+import HeaderImg from '../../assets/header.png'
+import { BiMap } from 'react-icons/bi'
+import data from './data'
 
 const Header = () => {
   return (
     <>
         <header className="header" id="header">
-          <h2>Header</h2>
+          <div className="container header__container">
+            <div className="header__profile">
+              <img src={HeaderImg} alt="Header image" />
+            </div>
+              <h3>márcio reis</h3>
+              <h4>desenvolvedor front-end</h4>
+              <span><i><BiMap /></i> são paulo, brasil</span>
+              <p>Tenho paixão por tecnologia, e ela me faz ter vontade de trabalhar e estudar cada vez mais para solucionar os mais variados tipos de problemas na área de desenvolviment</p>
+            <div className="header__cta">
+              <a href="#contact" className='btn'>Fale comigo</a>
+              <a href="#portfolio" className='btn'>Meus projetos</a>
+            </div>
+            <div className="header__socials">
+              {
+                data.map((item, index) => <a key={index} href={item.link} target='_blank'>{item.icon}</a>)
+              }
+            </div>
+          </div>
         </header>
     </>
   )
---
+++ src/sections/header/data.js
@@ -0,0 +1,9 @@
+
+const data = [
+    {id: 1, link: 'https://www.linkedin.com/in/devmarcioreis/', icon: 'Link'},
+    {id: 2, link: 'https://github.com/dev-marcioreis', icon: 'Git'},
+    {id: 3, link: 'https://api.whatsapp.com/send?1=pt_BR&phone=5511994341245', icon: 'Whats'},
+    {id: 4, link: 'https://t.me/devmarcioreis', icon: 'Tele'}
+]
+
+export default data
