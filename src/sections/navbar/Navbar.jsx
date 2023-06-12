564236d19f1ff1452c5cc8a6407f766660225596
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-06-12 15:15:36 -0300

    Feat: navbar section

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import './navbar.css'
+import Logo from '../../assets/logo.png'
+import data from './data'
+
+import { MdInvertColors as ThemeIcon } from 'react-icons/md'
 
 const Navbar = () => {
   return (
     <>
-        <nav className="navbar" id="navbar">
-          <h2>Navbar</h2>
+        <nav className="navbar">
+          <div className='container'>
+            <div className="navbar__container">
+              <a href="index.html" className="navbar__logo">
+                <img src={Logo} alt="Logo image" />
+              </a>
+              <ul className='navbar__menu'>
+                {
+                  data.map((item, index) => <li key={index}><a href={item.link}>{item.title}</a></li>)
+                }
+              </ul>
+              <button id='theme__icon'>
+                <ThemeIcon />
+              </button>
+            </div>
+          </div>
         </nav>
     </>
   )
---
+++ src/sections/navbar/data.js
@@ -0,0 +1,8 @@
+const data = [
+    {id: 1, link: '#', title: 'Início'},
+    {id: 2, link: '#bout', title: 'Sobre'},
+    {id: 3, link: '#portfolio', title: 'Portfólio'},
+    {id: 4, link: '#contact', title: 'Fale comigo'}
+]
+
+export default data
